820823e089a8ae2cc8157df45b5ad2c3fe96f7ef
IanZGray <ianzacharygray@gmail.com>
2023-10-20 15:06:07 -0400

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # 3dFun
+This is a 3d playground I made using Spline. Just experimenting on different ways to encorporate 3D environments in web. 
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/t4V49kBW6Q4o1aqO/scene.splinecode');
